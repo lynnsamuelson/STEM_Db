45aa334f5a00211526815c3b73fcbee2f1068a57
lynnsamuelson <lynn.samuelson@gmail.com>
2016-04-08 09:03:24 -0500

    seet-up initial views for api controller

---
+++ STEM_Db/Controllers/ExperimentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STEM_Db.Controllers
+{
+    public class ExperimentController : Controller
+    {
+        // GET: Experiment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ STEM_Db/Controllers/STEM_DbController.cs
@@ -0,0 +1,50 @@
+﻿using STEM_Db.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace STEM_Db.Controllers
+{
+    public class STEM_DbController : ApiController
+    {
+        public STEM_DbRepository Repo { get; set; }
+        public STEM_DbController() : base()
+        {
+            Repo = new STEM_DbRepository();
+        }
+
+        public STEM_DbController(STEM_DbRepository _repo)
+        {
+            Repo = _repo;
+        }
+        // GET: api/STEM_Db
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/STEM_Db/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/STEM_Db
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/STEM_Db/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/STEM_Db/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
